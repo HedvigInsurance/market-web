0f2b14c9192344f96e6977f941b0837c92c377c3
Isaac Sennerholt <isaacsennerholt@Isaacs-MacBook-Pro.local>
2018-11-12 17:22:53 +0100

    Format code in entry file

--- src/server/entry.tsx
+++ src/server/entry.tsx
@@ -2,11 +2,13 @@ import * as dotenv from 'dotenv'
 dotenv.config()
 
 import 'source-map-support/register'
+
 import {
   // tslint:disable-line ordered-imports
   createKoaServer,
   getScriptLocation,
 } from '@hedviginsurance/web-survival-kit'
+
 import { renderStylesToString } from 'emotion-server'
 import * as Koa from 'koa'
 import * as path from 'path'
@@ -18,6 +20,7 @@ import { FilledContext, HelmetProvider } from 'react-helmet-async'
 import { App } from '../App'
 import { routes } from '../routes'
 import { appLogger } from './logging'
+
 import {
   logRequestMiddleware,
   setLoggerMiddleware,
@@ -28,23 +31,23 @@ const scriptLocation = getScriptLocation({
   statsLocation: path.resolve(__dirname, 'assets'),
   webpackPublicPath: process.env.WEBPACK_PUBLIC_PATH || '',
 })
+
 const template = (body: string, helmet: FilledContext['helmet']) => `
-<!doctype html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  ${helmet.title}
-  ${helmet.link}
-  ${helmet.meta}
-</head>
-<body>
-  <div id="react-root">${body}</div>
-  
-  <script src="${scriptLocation}"></script>
-</body>
-</html>
+  <!doctype html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    ${helmet.title}
+    ${helmet.link}
+    ${helmet.meta}
+  </head>
+  <body>
+    <div id="react-root">${body}</div>
+    <script src="${scriptLocation}"></script>
+  </body>
+  </html>
 `
 
 const getPage: Koa.Middleware = async (ctx) => {
@@ -71,6 +74,7 @@ const getPage: Koa.Middleware = async (ctx) => {
 
   ctx.body = template(reactBody, (helmetContext as FilledContext).helmet)
 }
+
 const getPort = () => (process.env.PORT ? Number(process.env.PORT) : 8030)
 
 appLogger.info(`Booting server on ${getPort()} 👢`)
