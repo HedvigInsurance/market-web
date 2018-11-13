179ce5a93628d7dee4a16447e9832ef7cd682dc9
Johan Palmfjord <johan.palmfjord@hedvig.com>
2018-11-13 14:46:23 +0100

    Improve typings and add cannonical links

--- src/storyblok/StoryContainer.tsx
+++ src/storyblok/StoryContainer.tsx
@@ -1,8 +1,26 @@
 import { Container } from 'constate'
 import * as React from 'react'
 
+export interface BodyStory {
+  name: string
+  created_at: string
+  published_at: string
+  id: string
+  uuid: string
+  slug: string
+  full_slug: string
+  content: {
+    _uid: string
+    component: 'page'
+    body: ReadonlyArray<{
+      _uid: string
+      component: string
+    }>
+  }
+}
+
 export interface WithStory {
-  story: any
+  story: BodyStory
 }
 
 export interface StoryContainerProps {
