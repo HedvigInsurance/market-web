a60c6caee9f2980636bb88e04fa090e00aaddc96
Johan Palmfjord <johan.palmfjord@hedvig.com>
2018-11-15 11:47:22 +0100

    Prep prod and heroku with middlewares etc

---
+++ src/server/config/helmetConfig.ts
@@ -0,0 +1,52 @@
+import { IHelmetConfiguration } from 'helmet'
+import * as uuidV4 from 'uuid/v4'
+
+const defaultSrc = [
+  "'self'",
+  'cdn.hedvig.com',
+  'www.googletagmanager.com',
+  'https://*.hotjar.com',
+  'wss://*.hotjar.com',
+  'https://*.intercom.io',
+  'https://*.intercomcdn.com',
+  'https://*.intercomassets.com',
+  'https://*.intercomusercontent.com',
+  'wss://*.intercom.io',
+  'wss://*.intercomcdn.com',
+  'wss://*.intercomassets.com',
+  'wss://*.intercomusercontent.com',
+  'app.getsentry.com',
+  'www.google-analytics.com',
+  '*.facebook.net',
+  '*.facebook.com',
+  '*.doubleclick.net',
+]
+
+export const helmetConfig = (): IHelmetConfiguration => ({
+  contentSecurityPolicy: {
+    directives: {
+      defaultSrc,
+      scriptSrc: [
+        ...defaultSrc,
+        "'unsafe-eval'",
+        'browser.sentry-cdn.com',
+        'cdn.segment.com',
+        'www.googletagmanager.com',
+        // tslint:disable-next-line variable-name
+        (_request: unknown, response: any) => {
+          response.cspNonce = uuidV4()
+          return `'nonce-${(response as any).cspNonce}'`
+        },
+      ],
+      connectSrc: [
+        ...defaultSrc,
+        'https://api.segment.io',
+        'https://sentry.io',
+      ],
+      styleSrc: [...defaultSrc, "'unsafe-inline'"],
+      upgradeInsecureRequests: true,
+      objectSrc: ["'none'"],
+      reportUri: process.env.CSP_REPORT_ENDPOINT || '/_report-csp-violation',
+    },
+  },
+})
