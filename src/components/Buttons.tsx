088c966a3386738697a1e51b520adf27ad0a7aab
Isaac Sennerholt <isaacsennerholt@Isaacs-MacBook-Pro.local>
2018-11-16 07:29:33 +0100

    Update CallToActionSection

---
+++ src/components/Buttons.tsx
@@ -0,0 +1,39 @@
+import { colors } from '@hedviginsurance/brand'
+import styled from 'react-emotion'
+
+const buttonSizes = {
+  sm: '10px 20px',
+  md: '15px 40px',
+}
+
+interface ButtonProps {
+  size: keyof typeof buttonSizes
+}
+
+const Button = styled('button')(({ size = 'md' }: ButtonProps) => ({
+  padding: buttonSizes[size],
+  margin: 0,
+  borderRadius: 30,
+  cursor: 'pointer',
+  boxSizing: 'border-box',
+  display: 'inline-block',
+  flex: '0 1 auto',
+  flexBasis: 'auto',
+  font: 'inherit',
+  position: 'relative',
+  textAlign: 'center',
+  textDecoration: 'none',
+  userSelect: 'none',
+}))
+
+export const FilledButtonComponent = styled(Button)({
+  backgroundColor: colors.GREEN,
+  border: 'none',
+  color: colors.WHITE,
+})
+
+export const OutlinedButtonComponent = styled(Button)({
+  color: colors.GREEN,
+  backgroundColor: 'transparent',
+  border: `2px solid ${colors.GREEN}`,
+})
