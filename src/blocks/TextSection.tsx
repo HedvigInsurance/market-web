9ca35e63d44d328f8de68d2a0cb6a5f04f5619c2
Isaac Sennerholt <isaacsennerholt@Isaacs-MacBook-Pro.local>
2018-11-19 13:48:07 +0100

    Add TextSection block

---
+++ src/blocks/TextSection.tsx
@@ -0,0 +1,92 @@
+import * as React from 'react'
+import styled from 'react-emotion'
+import { BaseBlockProps, MarkdownHtmlComponent } from './BaseBlockProps'
+
+const SectionComponent = styled('section')(
+  ({ backgroundColor }: { backgroundColor: string }) => ({
+    width: '100%',
+    background: backgroundColor,
+    paddingTop: '120px',
+    paddingBottom: '120px',
+  }),
+)
+
+const ContentComponent = styled('div')(
+  ({ textPosition }: { textPosition: string }) => ({
+    display: 'flex',
+    flexDirection: textPosition === 'center' ? 'column' : 'row',
+    justifyContent: textPosition === 'left' ? 'space-between' : 'center',
+    alignItems: 'center',
+    textAlign: textPosition === 'center' ? 'center' : 'left',
+    width: '100%',
+  }),
+)
+
+const TitleComponent = styled('h2')(
+  ({
+    titleColor,
+    textPosition,
+  }: {
+    titleColor: string
+    textPosition: string
+  }) => ({
+    color: titleColor,
+    fontSize: '48px',
+    marginRight: textPosition === 'left' ? 'auto' : 0,
+    marginLeft: textPosition === 'right' ? 'auto' : 0,
+    width: '100%',
+    maxWidth: '50%',
+  }),
+)
+
+const ParagraphComponent = styled('div')(
+  ({
+    paragraphColor,
+    textPosition,
+  }: {
+    paragraphColor: string
+    textPosition: string
+  }) => ({
+    display: textPosition === 'right' ? 'none' : 'block',
+    marginTop: textPosition === 'center' ? '25px' : 0,
+    maxWidth: textPosition === 'left' ? '40%' : '100%',
+    width: '100%',
+    color: paragraphColor,
+    fontSize: '20px',
+  }),
+)
+
+interface TextSectionInterface extends BaseBlockProps {
+  title: string
+  titleColor: string
+  paragraphColor: string
+  textPosition: string
+  paragraph: MarkdownHtmlComponent
+  backgroundColor: string
+}
+
+export const TextSection: React.SFC<TextSectionInterface> = ({
+  title = '',
+  titleColor = '',
+  paragraphColor = '',
+  textPosition = '',
+  paragraph = {},
+  backgroundColor = '',
+}) => {
+  return (
+    <SectionComponent backgroundColor={backgroundColor}>
+      <ContentComponent className="Container" textPosition={textPosition}>
+        <TitleComponent titleColor={titleColor} textPosition={textPosition}>
+          {title}
+        </TitleComponent>
+        <ParagraphComponent
+          paragraphColor={paragraphColor}
+          textPosition={textPosition}
+          dangerouslySetInnerHTML={{
+            __html: paragraph.html,
+          }}
+        />
+      </ContentComponent>
+    </SectionComponent>
+  )
+}
--- src/components/GlobalStyles.tsx
+++ src/components/GlobalStyles.tsx
@@ -21,22 +21,18 @@ body {
 h1, h2, h3, h4, h5, h6 {
   font-family: ${fonts.SORAY}, sans-serif;
   font-kerning: none;
-<<<<<<< HEAD
-  margin: 1.414rem 0 0.5rem;
   font-weight: inherit;
   line-height: 1.2;
-=======
   margin: 0;
   padding: 0;
->>>>>>> Add ImageTextSection block
 }
 
 p {
-  margin-bottom: 1.3rem;
+  margin: 0;
+  padding: 0;
 }
 
 h1 {
-  margin-top: 0;
   font-size: 3.998rem;
 }
 h2 {
@@ -48,7 +44,6 @@ h3 {
 h4 {
   font-size: 1.414rem;
 }
-
 
 img {
   max-width: 100%;
