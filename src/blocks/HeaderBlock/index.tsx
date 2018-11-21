4d5af8fce3725138a1e0fc7dd7718343c0ac2785
Johan Palmfjord <johan.palmfjord@hedvig.com>
2018-11-21 17:44:46 +0100

    Make transparent header work when scrolling

--- src/blocks/HeaderBlock/index.tsx
+++ src/blocks/HeaderBlock/index.tsx
@@ -2,6 +2,7 @@ import { colors } from '@hedviginsurance/brand'
 import { Container } from 'constate'
 import * as React from 'react'
 import styled from 'react-emotion'
+import { Mount, Unmount } from 'react-lifecycle-components'
 import { ContentWrapper, ErrorBlock } from '../../components/blockHelpers'
 import { ButtonLink } from '../../components/buttons'
 import { Togglable } from '../../components/containers/Togglable'
@@ -15,6 +16,9 @@ export const WRAPPER_HEIGHT = '5rem'
 export const HEADER_VERTICAL_PADDING = '1.5rem'
 export const TOGGLE_TRANSITION_TIME = 250
 
+const isBelowScrollThreshold = () =>
+  typeof window !== 'undefined' && window.scrollY > 20
+
 const Wrapper = styled('div')(
   ({ inverse, open }: { inverse: boolean; open: boolean }) => ({
     position: 'fixed',
@@ -22,7 +26,8 @@ const Wrapper = styled('div')(
     left: 0,
     right: 0,
     zIndex: 100,
-    color: inverse ? colors.WHITE : undefined,
+    color: inverse && !isBelowScrollThreshold() ? colors.WHITE : 'inherit',
+    transition: 'color 300ms',
 
     [TABLET_BP_DOWN]: {
       bottom: open ? 0 : undefined,
@@ -32,16 +37,20 @@ const Wrapper = styled('div')(
 const Filler = styled('div')({
   height: WRAPPER_HEIGHT,
 })
-const HeaderBackgroundFiller = styled('div')({
-  position: 'absolute',
-  top: 0,
-  left: 0,
-  right: 0,
-  zIndex: -1,
-  height: WRAPPER_HEIGHT,
-  backgroundColor: colors.WHITE,
-  boxShadow: '0px 1px 2px rgba(0, 0, 0, 0.1), 0px 2px 5px rgba(0, 0, 0, 0.1)',
-})
+const HeaderBackgroundFiller = styled('div')(
+  ({ transparent }: { transparent: boolean }) => ({
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    zIndex: -1,
+    height: WRAPPER_HEIGHT,
+    backgroundColor: colors.WHITE,
+    opacity: transparent && !isBelowScrollThreshold() ? 0 : 1,
+    boxShadow: '0px 1px 2px rgba(0, 0, 0, 0.1), 0px 2px 5px rgba(0, 0, 0, 0.1)',
+    transition: 'opacity 300ms',
+  }),
+)
 
 const InnerHeaderWrapper = styled('div')({
   display: 'flex',
@@ -111,60 +120,123 @@ interface HeaderBlockProps extends BaseBlockProps {
   inverse_colors: boolean
 }
 
-const Header: React.FunctionComponent<
+class Header extends React.PureComponent<
   { story: GlobalStory } & HeaderBlockProps
-> = ({ story, is_transparent, inverse_colors }) => (
-  <>
-    {!is_transparent && <Filler />}
+> {
+  private backgroundFillerRef: null | HTMLDivElement = null
+  private wrapperRef: null | HTMLDivElement = null
 
-    <Togglable>
-      {({ isOpen, isClosing, toggleOpen }) => (
-        <Wrapper
-          inverse={is_transparent && inverse_colors}
-          open={isOpen || isClosing}
-        >
-          {!is_transparent && <HeaderBackgroundFiller />}
-          <ContentWrapper>
-            <InnerHeaderWrapper>
-              <LogoLink href="/">
-                <HedvigWordmark height={30} />
-              </LogoLink>
+  public render() {
+    return (
+      <>
+        <>
+          <Mount
+            on={() => {
+              if (!this.props.is_transparent) {
+                return
+              }
 
-              <Burger
-                isOpen={isOpen}
-                isClosing={isClosing}
-                onClick={toggleOpen}
-                preventInverse={inverse_colors && isOpen}
-              />
-              <Menu open={isOpen}>
-                {(story.content.header_menu_items || []).map((menuItem) => (
-                  <MenuLink
-                    key={menuItem._uid}
-                    href={getStoryblokLinkUrl(menuItem.link)}
-                  >
-                    {menuItem.label}
-                  </MenuLink>
-                ))}
+              setTimeout(() => {
+                this.updateHeaderTransparency() // update the scroll animation after mount because the server doesn't know scroll position
+              }, 1)
+              window.addEventListener('scroll', this.updateHeaderTransparency)
+            }}
+          />
+          <Unmount
+            on={() => {
+              if (!this.props.is_transparent) {
+                return
+              }
 
-                {story.content.show_cta && (
-                  <ButtonWrapper>
-                    <ButtonLink
-                      size="sm"
-                      bold
-                      href={getStoryblokLinkUrl(story.content.cta_link)}
-                    >
-                      {story.content.cta_label}
-                    </ButtonLink>
-                  </ButtonWrapper>
-                )}
-              </Menu>
-            </InnerHeaderWrapper>
-          </ContentWrapper>
-        </Wrapper>
-      )}
-    </Togglable>
-  </>
-)
+              window.removeEventListener(
+                'scroll',
+                this.updateHeaderTransparency,
+              )
+            }}
+          />
+          {!this.props.is_transparent && <Filler />}
+
+          <Togglable>
+            {({ isOpen, isClosing, toggleOpen }) => (
+              <Wrapper
+                inverse={this.props.is_transparent && this.props.inverse_colors}
+                open={isOpen || isClosing}
+                innerRef={(r) => {
+                  this.wrapperRef = r
+                }}
+              >
+                <HeaderBackgroundFiller
+                  transparent={this.props.is_transparent}
+                  innerRef={(r) => {
+                    this.backgroundFillerRef = r
+                  }}
+                />
+                <ContentWrapper>
+                  <InnerHeaderWrapper>
+                    <LogoLink href="/">
+                      <HedvigWordmark height={30} />
+                    </LogoLink>
+
+                    <Burger
+                      isOpen={isOpen}
+                      isClosing={isClosing}
+                      onClick={toggleOpen}
+                      preventInverse={this.props.inverse_colors && isOpen}
+                    />
+                    <Menu open={isOpen}>
+                      {(this.props.story.content.header_menu_items || []).map(
+                        (menuItem) => (
+                          <MenuLink
+                            key={menuItem._uid}
+                            href={getStoryblokLinkUrl(menuItem.link)}
+                          >
+                            {menuItem.label}
+                          </MenuLink>
+                        ),
+                      )}
+
+                      {this.props.story.content.show_cta && (
+                        <ButtonWrapper>
+                          <ButtonLink
+                            size="sm"
+                            bold
+                            href={getStoryblokLinkUrl(
+                              this.props.story.content.cta_link,
+                            )}
+                          >
+                            {this.props.story.content.cta_label}
+                          </ButtonLink>
+                        </ButtonWrapper>
+                      )}
+                    </Menu>
+                  </InnerHeaderWrapper>
+                </ContentWrapper>
+              </Wrapper>
+            )}
+          </Togglable>
+        </>
+      </>
+    )
+  }
+
+  private updateHeaderTransparency = () => {
+    if (!this.backgroundFillerRef || !this.wrapperRef) {
+      return
+    }
+
+    if (isBelowScrollThreshold()) {
+      this.backgroundFillerRef.style.opacity = '1'
+      this.wrapperRef.style.color = 'inherit'
+      return
+    }
+
+    this.backgroundFillerRef!.style.opacity = '0'
+
+    if (this.props.inverse_colors) {
+      this.wrapperRef!.style.color = colors.WHITE
+    }
+  }
+}
 
 export const HeaderBlock: React.FunctionComponent<HeaderBlockProps> = (
   headerBlockProps,
