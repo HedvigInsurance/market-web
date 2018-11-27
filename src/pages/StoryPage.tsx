bd0b4e1013b691012270ed082eee89ec906db653
Johan Palmfjord <johan.palmfjord@hedvig.com>
2018-11-27 15:47:53 +0100

    Add _all_ the tracking scripts 🎉 😢

--- src/pages/StoryPage.tsx
+++ src/pages/StoryPage.tsx
@@ -2,56 +2,18 @@ import * as React from 'react'
 import Helmet from 'react-helmet-async'
 import { getBlockComponent } from '../blocks'
 import { BodyStory, StoryContainer } from '../storyblok/StoryContainer'
+import { getMeta } from '../utils/meta'
 
 const getBlocksOrDefault = (story: BodyStory) =>
   (story && story.content && story.content.body) || []
 
-export const StoryPage: React.FunctionComponent = () => (
+export const StoryPage: React.FunctionComponent<{ nonce?: string }> = ({
+  nonce,
+}) => (
   <StoryContainer>
     {({ story }) => (
       <>
-        <Helmet>
-          <title>{story.name}</title>
-          {typeof window === 'undefined' && (
-            <link
-              rel="canonical"
-              href={`${process.env.HOST}/${story.full_slug.replace(
-                /\/?home$/,
-                '',
-              )}`}
-            />
-          )}
-          {story.content.robots && (
-            <meta name="robots" content={story.content.robots} />
-          )}
-          {story.content.seo_meta_title && (
-            <meta name="title" content={story.content.seo_meta_title} />
-          )}
-          {story.content.seo_meta_description && (
-            <meta
-              name="description"
-              content={story.content.seo_meta_description}
-            />
-          )}
-          {story.content.seo_meta_og_title && (
-            <meta
-              property="og:title"
-              content={story.content.seo_meta_og_title}
-            />
-          )}
-          {story.content.seo_meta_og_description && (
-            <meta
-              property="og:description"
-              content={story.content.seo_meta_og_description}
-            />
-          )}
-          {story.content.seo_meta_og_image && (
-            <meta
-              property="og:image"
-              content={story.content.seo_meta_og_image}
-            />
-          )}
-        </Helmet>
+        <Helmet>{getMeta({ story, nonce })}</Helmet>
 
         {getBlocksOrDefault(story).map((block) => {
           const BlockComponent = getBlockComponent(block.component)
--- src/server/config.ts
+++ src/server/config.ts
@@ -4,9 +4,13 @@ dotenv.config()
 export interface Config {
   forceHost?: string
   storyblokApiToken: string
+  segmentApiKey: string
+  branchApiKey: string
 }
 
 export const config: Config = {
   forceHost: process.env.FORCE_HOST,
   storyblokApiToken: process.env.STORYBLOK_API_TOKEN!,
+  segmentApiKey: process.env.SEGMENT_API_KEY || '',
+  branchApiKey: process.env.BRANCH_API_KEY || '',
 }
