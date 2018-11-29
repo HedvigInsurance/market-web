74a47fc1a63dae58dab0a3368d055057a568d98e
Johan Palmfjord <johan.palmfjord@hedvig.com>
2018-11-29 20:35:07 +0100

    Add an image block

--- src/blocks/index.tsx
+++ src/blocks/index.tsx
@@ -6,6 +6,7 @@ import { BulletPointBlock } from './BulletPointBlock'
 import { FooterBlock } from './FooterBlock'
 import { HeaderBlock } from './HeaderBlock'
 import { IconBulletPointBlock } from './IconBulletPointBlock'
+import { ImageBlock } from './ImageBlock'
 import { ImageMultiTextBlock } from './ImageMultiTextBlock'
 import { ImageTextBlock } from './ImageTextBlock'
 import { PlainTextBlock } from './PlainTextBlock'
@@ -22,6 +23,7 @@ const blockComponents = {
   image_multi_text_block: ImageMultiTextBlock,
   single_quote_block: SingleQuoteBlock,
   image_text_block: ImageTextBlock,
+  image_block: ImageBlock,
   icon_bullet_point_block: IconBulletPointBlock,
   title_paragraph_block: TitleParagraphBlock,
   title_cta_block: TitleCtaBlock,
