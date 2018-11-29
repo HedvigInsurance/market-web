6330dffcbddd19571c396a523a301b9f9ac55d5b
Johan Palmfjord <johan.palmfjord@hedvig.com>
2018-11-29 21:38:49 +0100

    Press block

---
+++ src/blocks/PressBlock/PressItem.tsx
@@ -0,0 +1,59 @@
+import { fonts } from '@hedviginsurance/brand'
+import * as React from 'react'
+import styled from 'react-emotion'
+
+import { PressItemProps } from '.'
+
+const Link = styled('a')({
+  textDecoration: 'none',
+})
+
+const PressItemContainer = styled('div')({
+  borderRadius: 5,
+  boxShadow: '-1px 0 10px rgba(0,0,0,0.14)',
+  padding: 20,
+  display: 'flex',
+  alignItems: 'center',
+  backgroundColor: 'white',
+})
+
+const Logo = styled('img')({
+  width: '75px',
+  height: '75px',
+  objectFit: 'contain',
+})
+
+const Content = styled('div')({
+  paddingLeft: 20,
+  display: 'flex',
+  flexDirection: 'column',
+})
+
+const Title = styled('span')({
+  fontSize: 20,
+  lineHeight: '21px',
+  fontFamily: fonts.SORAY,
+})
+
+const Text = styled('span')({
+  fontSize: 14,
+  lineHeight: '16px',
+  marginTop: 5,
+})
+
+export const PressItem: React.FunctionComponent<PressItemProps> = ({
+  logo,
+  link,
+  title,
+  text,
+}) => (
+  <Link href={link}>
+    <PressItemContainer>
+      <Logo src={logo} />
+      <Content>
+        <Title>{title}</Title>
+        <Text>{text}</Text>
+      </Content>
+    </PressItemContainer>
+  </Link>
+)
