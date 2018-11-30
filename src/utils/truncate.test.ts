fb25f1c3559a0dd2651cf50febecfc17a578c8b2
Johan Palmfjord <johan.palmfjord@hedvig.com>
2018-11-30 09:21:01 +0100

    Misc fixes

--- src/utils/truncate.test.ts
+++ src/utils/truncate.test.ts
@@ -1,22 +1,22 @@
-import { truncate } from './truncate';
+import { truncate } from './truncate'
 
 it("doesn't truncate text that is too short or equal to string length", () => {
-  const text = 'foobar';
+  const text = 'foobar'
 
-  expect(truncate(7)(text)).toEqual(text);
-  expect(truncate(6)(text)).toEqual(text);
-});
+  expect(truncate(7)(text)).toEqual(text)
+  expect(truncate(6)(text)).toEqual(text)
+})
 
 it('truncates text that is too long', () => {
-  const text = 'foobar';
-  expect(truncate(5)(text)).toEqual('fooba...');
-});
+  const text = 'foobar'
+  expect(truncate(5)(text)).toEqual('fooba...')
+})
 
 it('trims truncations', () => {
-  const text = 'foo bar';
-  expect(truncate(4)(text)).toEqual('foo...');
-});
+  const text = 'foo bar'
+  expect(truncate(4)(text)).toEqual('foo...')
+})
 
 it("doesn't 💥 on emtpy string null/unefined", () => {
-  expect(truncate(5)('')).toEqual('');
-});
+  expect(truncate(5)('')).toEqual('')
+})
