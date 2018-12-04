501c9a00a96c1f4c9e5f4a0d78220ddab03149b0
Johan Palmfjord <johan.palmfjord@hedvig.com>
2018-12-04 10:26:12 +0100

    Add Redirects for old blog posts

--- src/routes.ts
+++ src/routes.ts
@@ -45,6 +45,16 @@ export const tmpOldRoutes: string[] = [
 
 export const redirects: ReadonlyArray<[string, string, number]> = [
   [
+    '/blog/2018-11-16-meet-the-team-%E2%80%93-sofia',
+    '/blog/2018-11-16-meet-the-team-sofia',
+    301,
+  ],
+  [
+    '/blog/2018-10-12-hedvig-och-qasa-g%C3%B6r-det-enklare-och-tryggare-att-hyra-ut-din-bostad',
+    '/blog/2018-10-12-hedvig-och-qasa-gor-det-enklare-och-tryggare-att-hyra-ut-din-bostad',
+    301,
+  ],
+  [
     '/assets/press/hedvig-press-assets.zip',
     'https://cdn.hedvig.com/identity/hedvig-press-assets.zip',
     301,
