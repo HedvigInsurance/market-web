8d33161ec27d025ba3b587bbe2a22d802b7895f7
Johan Palmfjord <johan.palmfjord@hedvig.com>
2019-01-03 11:50:48 +0100

    Fix header breakpoint so its less broken

--- src/blocks/HeaderBlock/MenuItem.tsx
+++ src/blocks/HeaderBlock/MenuItem.tsx
@@ -3,9 +3,9 @@ import { Container } from 'constate'
 import * as React from 'react'
 import AnimateHeight from 'react-animate-height'
 import styled from 'react-emotion'
-import { TABLET_BP_DOWN } from '../../components/blockHelpers'
 import { MenuItem as MenuItemType } from '../../storyblok/StoryContainer'
 import { getStoryblokLinkUrl } from '../../utils/storyblok'
+import { TABLET_BP_DOWN } from './mobile';
 
 const MenuListItem = styled('li')({
   position: 'relative',
