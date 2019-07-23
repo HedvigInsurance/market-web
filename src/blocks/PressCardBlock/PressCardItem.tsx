1c6aba032e864dea812d6cc2c111c6585aa19e0f
Axel Backlund <axel@mocialy.com>
2019-07-23 10:32:15 +0200

    Resolving comments

--- src/blocks/PressCardBlock/PressCardItem.tsx
+++ src/blocks/PressCardBlock/PressCardItem.tsx
@@ -3,11 +3,15 @@ import styled from 'react-emotion'
 
 import { colors } from '@hedviginsurance/brand'
 import { PressCardItemProps } from '.'
-import { MOBILE_BP_DOWN } from '../../components/blockHelpers'
 import { DeferredImage } from '../../components/DeferredImage'
 import { getStoryblokImage } from '../../utils/storyblok'
 
-const PressItemContainer = styled('div')({
+const PressItemContainer = styled('div')(
+  ({
+    offWhite = false,
+  }: {
+    offWhite?: Boolean
+  }) => ({
   borderRadius: 8,
   padding: 24,
   margin: '0 20px',
@@ -16,22 +20,17 @@ const PressItemContainer = styled('div')({
   flexDirection: 'column',
   alignItems: 'center',
   justifyContent: 'center',
-  backgroundColor: 'white',
-})
+  backgroundColor: offWhite && offWhite ? colors.OFF_WHITE : 'white',
+}))
 
 const Logo = styled(DeferredImage)({
-  width: 80,
-  height: 80,
-  objectFit: 'contain',
-  [MOBILE_BP_DOWN]: {
-    width: 80,
-    height: 80,
-  },
+  height: '2rem',
+  marginTop: '10px'
 })
 
 const Text = styled('span')({
   fontSize: 18,
-  lineHeight: '24px',
+  lineHeight: 1.3,
   marginTop: 5,
   textAlign: 'center',
   color: colors.OFF_BLACK_DARK,
@@ -40,8 +39,9 @@ const Text = styled('span')({
 export const PressCardItem: React.FunctionComponent<PressCardItemProps> = ({
   logo,
   quote,
+  blockColor
 }) => (
-  <PressItemContainer>
+  <PressItemContainer offWhite={ blockColor && blockColor.color === 'standard' }>
     <Text>“{quote}”</Text>
     <Logo src={getStoryblokImage(logo)} />
   </PressItemContainer>
