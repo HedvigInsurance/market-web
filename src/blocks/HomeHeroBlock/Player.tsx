9cbfb2907ab4b5885d869977f47d9c12ba9b3f6f
Axel Backlund <axel@mocialy.com>
2019-08-04 16:59:14 +0200

    Adding background image option

--- src/blocks/HomeHeroBlock/Player.tsx
+++ src/blocks/HomeHeroBlock/Player.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react'
 import styled, { keyframes } from 'react-emotion'
+import MediaQuery from 'react-responsive'
+import { backgroundImageStyles } from '../../components/blockHelpers'
 
 interface BackgroundProps {
   backgroundColor: string
@@ -26,6 +28,13 @@ const HeightContainer = styled('div')(
   }),
 )
 
+const BackgroundImage = styled('div')(({ image }: { image: string }) => ({
+  height: '90vh',
+  overflow: 'hidden',
+  width: '100%',
+  ...backgroundImageStyles(image),
+}))
+
 const VideoWrapper = styled('div')({
   height: '90vh',
   overflow: 'hidden',
@@ -51,32 +60,62 @@ const Video = styled('video')({
 
 interface PlayerProps {
   videoRef: React.RefObject<HTMLVideoElement>
+  mobileVideoRef: React.RefObject<HTMLVideoElement>
+  baseMobileVideoUrl: string
   baseVideoUrl: string
+  desktopImage: string
+  mobileImage: string
 }
+
+interface VideoItemProps {
+  videoUrl: string
+  videoRef: React.RefObject<HTMLVideoElement>
+}
+
+const VideoItem: React.FunctionComponent<VideoItemProps> = ({
+  videoUrl,
+  videoRef,
+}) => (
+  <VideoWrapper>
+    <Video
+      poster={`${videoUrl}.png`}
+      innerRef={videoRef}
+      playsInline
+      autoPlay
+      muted={true}
+      loop={true}
+      controls={false}
+    >
+      <source src={`${videoUrl}.m3u8`} type="application/vnd.apple.mpegurl" />
+      <source src={`${videoUrl}.mp4`} type="video/mp4" />
+      <source src={`${videoUrl}.webm`} type="video/webm" />
+    </Video>
+  </VideoWrapper>
+)
 
 export const Player: React.SFC<PlayerProps & BackgroundProps> = ({
   videoRef,
+  mobileVideoRef,
+  desktopImage,
+  mobileImage,
   baseVideoUrl,
+  baseMobileVideoUrl,
   backgroundColor,
 }) => (
   <HeightContainer backgroundColor={backgroundColor}>
-    <VideoWrapper>
-      <Video
-        poster={`${baseVideoUrl}.png`}
-        innerRef={videoRef}
-        playsInline
-        autoPlay
-        muted={true}
-        loop={true}
-        controls={false}
-      >
-        <source
-          src={`${baseVideoUrl}.m3u8`}
-          type="application/vnd.apple.mpegurl"
-        />
-        <source src={`${baseVideoUrl}.mp4`} type="video/mp4" />
-        <source src={`${baseVideoUrl}.webm`} type="video/webm" />
-      </Video>
-    </VideoWrapper>
+    <MediaQuery query="(max-width: 700px)">
+      {baseMobileVideoUrl ? (
+        <VideoItem videoUrl={baseMobileVideoUrl} videoRef={mobileVideoRef} />
+      ) : (
+        mobileImage && <BackgroundImage image={mobileImage} />
+      )}
+    </MediaQuery>
+    <MediaQuery query="(min-width: 701px)">
+      {baseVideoUrl ? (
+        <VideoItem videoUrl={baseVideoUrl} videoRef={videoRef} />
+      ) : (
+        desktopImage && <BackgroundImage image={desktopImage} />
+      )}
+    </MediaQuery>
   </HeightContainer>
 )
