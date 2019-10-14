ef0fb7f098f019049c0d15a54a899994489b5964
Sam Pettersson <sam@sampettersson.com>
2019-10-14 17:08:17 +0200

    Prettier

--- src/components/AppLink.tsx
+++ src/components/AppLink.tsx
@@ -11,10 +11,12 @@ import { trackEvent } from '../utils/tracking/trackEvent'
 // Dynamically creating a link automatically copies over
 // any utm tags sent from ad networks
 interface AppLinkProps {
-  children: (props: {
-    handleClick: (e: React.MouseEvent<HTMLElement>) => void
-    link: string
-  }) => React.ReactNode
+  children: (
+    props: {
+      handleClick: (e: React.MouseEvent<HTMLElement>) => void
+      link: string
+    },
+  ) => React.ReactNode
   channel?: string
   campaign?: string
   tags?: ReadonlyArray<string>
@@ -28,58 +30,58 @@ interface AppLinkState {
 
 const AppLinkComponent: React.FunctionComponent<
   AppLinkProps & RouteComponentProps
-  > = (props) => (
-    <Container<AppLinkState, { setLink: (link: string) => void }>
-      initialState={{
-        link: undefined,
-      }}
-      actions={{ setLink: (link) => () => ({ link }) }}
-    >
-      {({ link, setLink }) => (
-        <>
-          <StoryContainer>
-            {({ story }) => (
-              <>
-                <Mount
-                  on={() => {
-                    const utmParams = Cookies.getJSON('utm-params') || {}
-                    const source = utmParams.source || props.channel
-                    const medium = utmParams.medium || props.feature
-                    const name = utmParams.name || props.campaign
-                    const content = utmParams.content || props.tags
-                    const keywords = utmParams.keywords || props.tags
+> = (props) => (
+  <Container<AppLinkState, { setLink: (link: string) => void }>
+    initialState={{
+      link: undefined,
+    }}
+    actions={{ setLink: (link) => () => ({ link }) }}
+  >
+    {({ link, setLink }) => (
+      <>
+        <StoryContainer>
+          {({ story }) => (
+            <>
+              <Mount
+                on={() => {
+                  const utmParams = Cookies.getJSON('utm-params') || {}
+                  const source = utmParams.source || props.channel
+                  const medium = utmParams.medium || props.feature
+                  const name = utmParams.name || props.campaign
+                  const content = utmParams.content || props.tags
+                  const keywords = utmParams.keywords || props.tags
 
-                    const lang = story ? story.lang : 'sv'
-                    const host = getPublicHost() || 'https://www.hedvig.com'
+                  const lang = story ? story.lang : 'sv'
+                  const host = getPublicHost() || 'https://www.hedvig.com'
 
-                    const desktopLink = `${host}/${
-                      lang === 'sv' ? '' : lang + '/'
-                      }new-member`
+                  const desktopLink = `${host}/${
+                    lang === 'sv' ? '' : lang + '/'
+                  }new-member`
 
-                    setLink(
-                      `https://hedvig.page.link/?link=${desktopLink}&utm_source=${source}&utm_medium=${medium}&utm_name=${name}&utm_content=${content}&utm_keywords=${keywords}`,
-                    )
-                  }}
-                >
-                  {null}
-                </Mount>
-                {props.children({
-                  link,
-                  handleClick: (e: React.MouseEvent<HTMLElement>) => {
-                    e.preventDefault()
-                    trackEvent('Click app link', {
-                      label: props.tags && props.tags.join(', '),
-                    })
-                    window.location.href = link
-                  },
-                })}
-              </>
-            )}
-          </StoryContainer>
-        </>
-      )}
-    </Container>
-  )
+                  setLink(
+                    `https://hedvig.page.link/?link=${desktopLink}&utm_source=${source}&utm_medium=${medium}&utm_name=${name}&utm_content=${content}&utm_keywords=${keywords}`,
+                  )
+                }}
+              >
+                {null}
+              </Mount>
+              {props.children({
+                link,
+                handleClick: (e: React.MouseEvent<HTMLElement>) => {
+                  e.preventDefault()
+                  trackEvent('Click app link', {
+                    label: props.tags && props.tags.join(', '),
+                  })
+                  window.location.href = link
+                },
+              })}
+            </>
+          )}
+        </StoryContainer>
+      </>
+    )}
+  </Container>
+)
 
 AppLinkComponent.defaultProps = {
   channel: 'organic',
