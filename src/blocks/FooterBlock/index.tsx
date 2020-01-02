f51ebeff8496004cc2a770537896a732801401c4
Johan Palmfjord <johan.palmfjord@hedvig.com>
2020-01-02 16:38:33 +0100

    Add an accept-language redirect

--- src/blocks/FooterBlock/index.tsx
+++ src/blocks/FooterBlock/index.tsx
@@ -111,7 +111,7 @@ export const FooterBlock: React.FunctionComponent<FooterBlockProps> = ({
                   <HedvigWordmark height={30} />
 
                   <LangSwitchersContainer>
-                    <LangSwitcher href="/">
+                    <LangSwitcher href="/?lang-redirect=disable">
                       <SweFlag />
                     </LangSwitcher>
                     <LangSwitcher href="/en">
