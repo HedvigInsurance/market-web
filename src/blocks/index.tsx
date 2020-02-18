9f66f27a70e976c1ca33d3a9a351012978f5904a
Johan Palmfjord <johan.palmfjord@hedvig.com>
2020-02-18 09:24:51 +0100

    Spring clean some unused blocks

--- src/blocks/index.tsx
+++ src/blocks/index.tsx
@@ -1,32 +1,25 @@
 import * as React from 'react'
 import { AboutUsHeroBlock } from './AboutUsHeroBlock'
 import { AccordionBlock } from './AccordionBlock'
-import { AnimatedCtaBlock } from './AnimatedCtaBlock'
-import { AnimatedPhoneBlock } from './AnimatedPhoneBlock'
 import { BackgroundVideoBlock } from './BackgroundVideoBlock'
 import { BaseBlockProps } from './BaseBlockProps'
 import { BulletPointBlock } from './BulletPointBlock'
 import { CardChecklistBulletPointBlock } from './CardChecklistBulletPointBlock'
 import { DownloadBlock } from './DownloadBlock'
-import { FactsBlock } from './FactsBlock'
 import { FooterBlock } from './FooterBlock'
 import { HeaderBlock } from './HeaderBlock'
-import { HedvigersBlock } from './HedvigersBlock'
 import { HeroVideoBlock } from './HeroVideoBlock'
 import { HomeHeroBlock } from './HomeHeroBlock'
 import { IconBulletPointBlock } from './IconBulletPointBlock'
-import { IconForestBlock } from './IconForestBlock'
 import { ImageBlock } from './ImageBlock'
 import { ImageMultiTextBlock } from './ImageMultiTextBlock'
 import { ImageTextBlock } from './ImageTextBlock'
 import { InvestorsBlock } from './InvestorsBlock'
-import { MediaLogosBlock } from './MediaLogosBlock'
 import { PlainTextBlock } from './PlainTextBlock'
 import { PressBlock } from './PressBlock'
 import { PressCardBlock } from './PressCardBlock'
 import { SingleQuoteBlock } from './SingleQuoteBlock'
 import { SpacerBlock } from './SpacerBlock'
-import { SwitcherBlock } from './SwitcherBlock'
 import { TitleCtaBlock } from './TitleCtaBlock'
 import { TitleParagraphBlock } from './TitleParagraphBlock'
 import { TitleTextCtaBlock } from './TitleTextCtaBlock'
@@ -38,19 +31,15 @@ const blockComponents = {
   accordion_block: AccordionBlock,
   bullet_point_block: BulletPointBlock,
   download_block: DownloadBlock,
-  facts_block: FactsBlock,
   image_multi_text_block: ImageMultiTextBlock,
   investors_block: InvestorsBlock,
-  hedvigers_block: HedvigersBlock,
   hero_video_block: HeroVideoBlock,
   home_hero_block: HomeHeroBlock,
   single_quote_block: SingleQuoteBlock,
   image_text_block: ImageTextBlock,
   title_text_cta_block: TitleTextCtaBlock,
   image_block: ImageBlock,
-  media_logos_block: MediaLogosBlock,
   icon_bullet_point_block: IconBulletPointBlock,
-  switcher_block: SwitcherBlock,
   title_paragraph_block: TitleParagraphBlock,
   title_cta_block: TitleCtaBlock,
   background_video_block: BackgroundVideoBlock,
@@ -59,10 +48,7 @@ const blockComponents = {
   trustpilot_block: TrustpilotBlock,
   spacer_block: SpacerBlock,
   footer_block: FooterBlock,
-  animated_phone_block: AnimatedPhoneBlock,
   card_checklist_bullet_point_block: CardChecklistBulletPointBlock,
-  animated_cta_block: AnimatedCtaBlock,
-  icon_forest_block: IconForestBlock,
   press_card_block: PressCardBlock,
 }
 
