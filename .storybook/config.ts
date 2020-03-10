92222506b3a5b49ff049dcc87f8bccc668ebb07c
gustaveen <gustaveen@gmail.com>
2020-03-10 15:04:23 +0100

    Add themeDecorator to storybook

--- .storybook/config.ts
+++ .storybook/config.ts
@@ -1,6 +1,11 @@
 import { configure } from '@storybook/react'
+import { addDecorator } from '@storybook/react'
+import { themeDecorator } from './decorators/themeDecorator'
+
 const req = require.context('../src', true, /\.stories\.tsx$/)
 function loadStories() {
   req.keys().forEach(req)
 }
 configure(loadStories, module)
+
+addDecorator(themeDecorator)
