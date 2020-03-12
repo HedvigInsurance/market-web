14e17c03cd6e3456ff5ff4f0186b300c4b3ab355
gustaveen <gustaveen@gmail.com>
2020-03-12 12:05:43 +0100

    Add storybook viewport addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,6 +4,7 @@ module.exports = {
     '@storybook/addon-backgrounds/register',
     '@storybook/addon-actions/register',
     '@storybook/addon-knobs/register',
+    '@storybook/addon-viewport/register',
     'storybook-addon-paddings',
   ],
   webpackFinal: async (config) => {
