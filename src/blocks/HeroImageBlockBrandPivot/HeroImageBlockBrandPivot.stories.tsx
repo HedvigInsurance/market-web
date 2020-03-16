14a9014bae92c61946ee6be405c1b099a5d6b3ad
Gustav <gustav.een@tink.se>
2020-03-16 13:57:47 +0100

    Add text color option to Hero

--- src/blocks/HeroImageBlockBrandPivot/HeroImageBlockBrandPivot.stories.tsx
+++ src/blocks/HeroImageBlockBrandPivot/HeroImageBlockBrandPivot.stories.tsx
@@ -1,13 +1,15 @@
+import { boolean, select, withKnobs } from '@storybook/addon-knobs'
 import * as React from 'react'
+import { MarkdownHtmlComponent, MinimalColorComponent } from '../BaseBlockProps'
 import {
   HeroImageBlockBrandPivot,
   HeroImageBlockBrandPivotProps,
 } from './HeroImageBlockBrandPivot'
-import { MarkdownHtmlComponent, MinimalColorComponent } from '../BaseBlockProps'
 
 export default {
   title: 'Blocks/HeroBlock',
   component: HeroImageBlockBrandPivot,
+  decorators: [withKnobs],
 }
 
 const heroText: MarkdownHtmlComponent = {
@@ -19,25 +21,57 @@ const heroText: MarkdownHtmlComponent = {
   plugin: 'markdown-html',
 }
 
-const color: MinimalColorComponent = {
+const standardInverse: MinimalColorComponent = {
   _uid: '6ecde11d-ba0a-48fb-9b7b-e6dbf31415d9',
   color: 'standard-inverse',
   plugin: 'hedvig_minimal_color_picker',
+}
+
+const colors = {
+  standard: {
+    _uid: '6ecde11d-ba0a-48fb-9b7b-e6dbf31415d9',
+    color: 'standard',
+    plugin: 'hedvig_minimal_color_picker',
+  },
+  standardInverse,
+  gray700: {
+    _uid: '6ecde11d-ba0a-48fb-9b7b-e6dbf31415d9',
+    color: 'gray700',
+    plugin: 'hedvig_minimal_color_picker',
+  },
+  gray500Inverse: {
+    _uid: '6ecde11d-ba0a-48fb-9b7b-e6dbf31415d9',
+    color: 'gray500-inverse',
+    plugin: 'hedvig_minimal_color_picker',
+  },
 }
 
 const heroProps: HeroImageBlockBrandPivotProps = {
   _uid: '5678',
   component: 'hero_block',
   headline: 'Säg hej till Hedvig',
-  indented_text: heroText,
+  text: heroText,
   image: '',
   image_mobile: '',
-  color: color,
 }
 
 const image = 'https://cdn.hedvig.com/www/referrals/referrals-clean.png'
 
-export const Default = () => <HeroImageBlockBrandPivot {...heroProps} />
+export const Default = () => (
+  <HeroImageBlockBrandPivot
+    {...heroProps}
+    color={select('color', colors, colors.standardInverse)}
+    text_color={select('Text color', colors, colors.standardInverse)}
+    useDisplayFont={boolean('Use display font', false)}
+  />
+)
 export const WithImage = () => (
-  <HeroImageBlockBrandPivot {...heroProps} image={image} image_mobile={image} />
+  <HeroImageBlockBrandPivot
+    {...heroProps}
+    image={image}
+    image_mobile={image}
+    color={select('color', colors, colors.standardInverse)}
+    text_color={select('Text color', colors, colors.standardInverse)}
+    useDisplayFont={boolean('Use display font', false)}
+  />
 )
