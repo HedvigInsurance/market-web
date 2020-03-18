c74d49aaec8d21ba80c1048eda58c6a2f8937fb2
Gustav <gustav.een@tink.se>
2020-03-18 17:54:53 +0100

    Add storybook example for Header block

---
+++ src/blocks/HeaderBlockBrandPivot/HeaderBlockBrandPivot.stories.tsx
@@ -0,0 +1,53 @@
+import styled from '@emotion/styled'
+import { colorsV3 } from '@hedviginsurance/brand'
+import { boolean, select, withKnobs } from '@storybook/addon-knobs'
+import * as React from 'react'
+import { globalStoryMock, minimalColorMap } from 'utils/storybook'
+import { Header } from '.'
+
+export default {
+  title: 'Blocks/HeaderBlock',
+  component: Header,
+  decorators: [withKnobs],
+}
+
+const headerBaseProps = {
+  _uid: '1234',
+  component: 'header',
+}
+
+const ScrollContainer = styled('div')({
+  height: '140vh',
+})
+
+export const Default = () => (
+  <div>
+    <Header
+      {...headerBaseProps}
+      story={globalStoryMock}
+      is_transparent={boolean('Is Transparent', true)}
+      inverse_colors={boolean('Inverse Colors (transparent header only)', true)}
+      cta_color={
+        minimalColorMap[
+          select('CTA color', Object.keys(minimalColorMap), 'standard-inverse')
+        ]
+      }
+      cta_style={select(
+        'CTA style',
+        ['outlined', 'filled', 'plain'],
+        'outlined',
+      )}
+    />
+    <ScrollContainer />
+  </div>
+)
+
+Default.story = {
+  parameters: {
+    backgrounds: [
+      { name: 'gray500', value: colorsV3.gray500, default: true },
+      { name: 'gray900', value: colorsV3.gray900 },
+      { name: 'white', value: colorsV3.white },
+    ],
+  },
+}
