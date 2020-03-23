97ffecad681eaa003fa3faa2936d3273a2186776
gustaveen <gustaveen@gmail.com>
2020-03-23 16:04:45 +0100

    Add wordmark option to HeadlineBlock

--- src/blocks/HeadlineBlock/HeadlineBlock.tsx
+++ src/blocks/HeadlineBlock/HeadlineBlock.tsx
@@ -1,14 +1,20 @@
 import styled from '@emotion/styled'
 import { fonts } from '@hedviginsurance/brand'
-import { BaseBlockProps } from 'blocks/BaseBlockProps'
-import { ContentWrapper, SectionWrapper } from 'components/blockHelpers'
+import { BrandPivotBaseBlockProps } from 'blocks/BaseBlockProps'
+import {
+  ContentWrapper,
+  SectionWrapper,
+  TABLET_BP_UP,
+} from 'components/blockHelpers'
+import { HedvigH } from 'components/icons/HedvigH'
 import React from 'react'
-import { textFlexPositionMap, TextPosition } from 'utils/textPosition'
+import { TextPosition } from 'utils/textPosition'
 
-interface HeadlineBlockProps extends BaseBlockProps {
+interface HeadlineBlockProps extends BrandPivotBaseBlockProps {
   text: string
   text_position: TextPosition
   use_display_font: boolean
+  show_hedvig_wordmark?: boolean
   font_size: 'xs' | 'sm' | 'md' | 'lg'
   element: 'h1' | 'h2' | 'h3' | 'h4'
 }
@@ -26,13 +32,33 @@ const createHeadline = (element: 'h1' | 'h2' | 'h3' | 'h4') =>
     size: 'xs' | 'sm' | 'md' | 'lg'
     useDisplayFont: boolean
   }>(({ textPosition, size, useDisplayFont }) => ({
-    display: 'flex',
-    justifyContent: textFlexPositionMap[textPosition],
+    position: 'relative',
+    textAlign: textPosition,
     fontSize: sizeMap[size],
     margin: 0,
     fontFamily: useDisplayFont ? `${fonts.EB_GARAMOND}, serif` : undefined,
     lineHeight: 1.5,
   }))
+
+const Wordmark = styled('div')({
+  display: 'inline-flex',
+  position: 'absolute',
+  marginTop: '0.625rem',
+  marginLeft: '0.5rem',
+
+  ['svg']: {
+    width: '1.25rem',
+    height: '1.25rem',
+  },
+
+  [TABLET_BP_UP]: {
+    marginTop: '1rem',
+    ['svg']: {
+      width: '2rem',
+      height: '2rem',
+    },
+  },
+})
 
 export const HeadlineBlock: React.FC<HeadlineBlockProps> = ({
   color,
@@ -40,6 +66,7 @@ export const HeadlineBlock: React.FC<HeadlineBlockProps> = ({
   text,
   text_position,
   use_display_font,
+  show_hedvig_wordmark,
   extra_styling,
   element,
   font_size,
@@ -56,10 +83,16 @@ export const HeadlineBlock: React.FC<HeadlineBlockProps> = ({
       <ContentWrapper brandPivot index={index}>
         <Headline
           textPosition={text_position}
-          dangerouslySetInnerHTML={{ __html: text }}
           size={font_size}
           useDisplayFont={use_display_font}
-        />
+        >
+          {text}
+          {show_hedvig_wordmark && (
+            <Wordmark>
+              <HedvigH />
+            </Wordmark>
+          )}
+        </Headline>
       </ContentWrapper>
     </SectionWrapper>
   )
