437f423e9f228622a28f0eb5a5ad1d9d75a51615
gustaveen <gustaveen@gmail.com>
2020-03-24 08:50:59 +0100

    Remove dash from quote author

--- src/blocks/QuoteBlockBrandPivot/QuoteBlockBrandPivot.stories.tsx
+++ src/blocks/QuoteBlockBrandPivot/QuoteBlockBrandPivot.stories.tsx
@@ -1,22 +1,12 @@
 import { boolean, select, text, withKnobs } from '@storybook/addon-knobs'
 import React from 'react'
 import { minimalColorMap } from 'utils/storybook'
-import { MarkdownHtmlComponent } from '../BaseBlockProps'
 import { QuoteBlockBrandPivot } from './QuoteBlockBrandPivot'
 
 export default {
   title: 'Blocks/QuoteBlock',
   component: QuoteBlockBrandPivot,
   decorators: [withKnobs],
-}
-
-const heroText: MarkdownHtmlComponent = {
-  _uid: '1234',
-  html:
-    '<p>Hedvig är framtidens försäkring. Medan traditionella försäkringsbolag försäkrar prylar, hjälper vi människor. Vi finns specifikt för dig i behov av omedelbar hjälp, oavsett var du befinner dig.</p>',
-  original:
-    '<p>Hedvig är framtidens försäkring. Medan traditionella försäkringsbolag försäkrar prylar, hjälper vi människor. Vi finns specifikt för dig i behov av omedelbar hjälp, oavsett var du befinner dig.</p>',
-  plugin: 'markdown-html',
 }
 
 const quoteProps = {
@@ -38,7 +28,7 @@ export const Default = () => (
           'Quote',
           '”My boyfriend got sick while I was on vacation, Hedvig flew me home and also kept in touch with the hospital. I’m overwhelmed.”',
         ),
-        author: text('Author', 'Cecilia Jansson'),
+        author: text('Author', '– Cecilia Jansson'),
         large_quote: boolean('Large quote', false),
       },
     ]}
