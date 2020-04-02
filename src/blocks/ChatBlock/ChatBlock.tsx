0673c63c15c78abe714c22ed6d2857d2c90418b0
gustaveen <gustaveen@gmail.com>
2020-04-02 14:50:43 +0200

    Add slide in animation for chat bubbles

--- src/blocks/ChatBlock/ChatBlock.tsx
+++ src/blocks/ChatBlock/ChatBlock.tsx
@@ -3,15 +3,18 @@ import { colorsV3 } from '@hedviginsurance/brand'
 import {
   ContentWrapper,
   LAPTOP_BP_UP,
+  MOBILE_BP_UP,
   SectionWrapper,
-  TABLET_BP_UP,
 } from 'components/blockHelpers'
 import React from 'react'
+import ReactVisibilitySensor from 'react-visibility-sensor'
 import { BrandPivotBaseBlockProps, TextField } from '../BaseBlockProps'
 
 export interface ChatBlockProps extends BrandPivotBaseBlockProps {
   messages: TextField[]
 }
+
+export const TABLET_BP_UP = '@media (min-width: 768px)'
 
 const ChatItems = styled.div`
   display: flex;
@@ -19,13 +22,32 @@ const ChatItems = styled.div`
   align-items: flex-start;
 `
 
-const ChatItem = styled.div`
+const ChatItem = styled.div<{ visible: boolean }>`
   display: inline-flex;
   max-width: calc(100% - 5rem);
   margin-top: 0.75rem;
   margin-bottom: 0.75rem;
   padding: 0.75rem 1rem;
   border-radius: 0.5rem;
+  opacity: ${(props) => (props.visible ? 1 : 0)};
+  transition: opacity 800ms, transform 500ms;
+
+  &:nth-of-type(odd) {
+    background-color: ${colorsV3.gray800};
+    transform: ${(props) =>
+      props.visible ? 'translateX(0)' : 'translateX(-10%)'};
+  }
+
+  &:nth-of-type(even) {
+    align-self: flex-end;
+    background-color: ${colorsV3.gray700};
+    transform: ${(props) =>
+      props.visible ? 'translateX(0)' : 'translateX(10%)'};
+  }
+
+  ${MOBILE_BP_UP} {
+    max-width: 60%;
+  }
 
   ${TABLET_BP_UP} {
     max-width: 49%;
@@ -34,6 +56,16 @@ const ChatItem = styled.div`
     padding: 1.5rem;
     font-size: 1.5rem;
     border-radius: 1rem;
+
+    &:nth-of-type(odd) {
+      transform: ${(props) =>
+        props.visible ? 'translateX(0)' : 'translateX(-5%)'};
+    }
+
+    &:nth-of-type(even) {
+      transform: ${(props) =>
+        props.visible ? 'translateX(0)' : 'translateX(5%)'};
+    }
   }
 
   ${LAPTOP_BP_UP} {
@@ -44,29 +76,31 @@ const ChatItem = styled.div`
     line-height: 1.25;
     border-radius: 1.5rem;
   }
-
-  &:nth-child(odd) {
-    background-color: ${colorsV3.gray800};
-  }
-
-  &:nth-child(even) {
-    align-self: flex-end;
-    background-color: ${colorsV3.gray700};
-  }
 `
 
 export const ChatBlock: React.FC<ChatBlockProps> = ({
   color,
-  index,
   size,
   messages,
 }) => {
   return (
     <SectionWrapper brandPivot colorComponent={color} size={size}>
-      <ContentWrapper brandPivot fullWidth index={index}>
+      <ContentWrapper brandPivot fullWidth>
         <ChatItems>
           {messages.map((message) => (
-            <ChatItem key={message._uid}>{message.text}</ChatItem>
+            <ReactVisibilitySensor
+              partialVisibility
+              key={message._uid}
+              offset={{
+                top: -500,
+                bottom:
+                  typeof window !== 'undefined' ? window.innerHeight / 6 : 0,
+              }}
+            >
+              {({ isVisible }) => (
+                <ChatItem visible={isVisible}>{message.text}</ChatItem>
+              )}
+            </ReactVisibilitySensor>
           ))}
         </ChatItems>
       </ContentWrapper>
