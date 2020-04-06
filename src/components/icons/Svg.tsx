6ccdbd8856b34789e2c9ee51c6fd9b1792f7fece
gustaveen <gustaveen@gmail.com>
2020-04-07 01:51:52 +0200

    Add new icons

--- src/components/icons/Svg.tsx
+++ src/components/icons/Svg.tsx
@@ -1,14 +1,31 @@
 import styled from '@emotion/styled'
 
+type Direction = 'up' | 'down' | 'left' | 'right'
 export interface SvgProps {
   fill?: string
   size?: number | string
+  direction?: Direction
+}
+
+const getDirection = (direction: Direction) => {
+  return `
+  ${
+    direction === 'right'
+      ? 'rotate(-90deg)'
+      : direction === 'left'
+      ? 'rotate(90deg)'
+      : direction === 'up'
+      ? 'scaleY(-1)'
+      : direction === 'down' && 'scaleY(1)'
+  };
+`
 }
 
 export const Svg = styled('svg')<SvgProps>(
-  ({ fill = 'currentColor', size = '1em' }) => ({
+  ({ fill = 'currentColor', size = '1em', direction = 'down' }) => ({
     fill,
     width: size,
     height: size,
+    transform: getDirection(direction),
   }),
 )
