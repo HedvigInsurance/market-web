acac4a158b7239866c51521118784f9610b24fc3
gustaveen <gustaveen@gmail.com>
2020-04-07 16:05:55 +0200

    Remove framer-motion and add native animation

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,6 +1,5 @@
 import styled from '@emotion/styled'
 import { colorsV3 } from '@hedviginsurance/brand'
-import { motion } from 'framer-motion'
 import React from 'react'
 import { createPortal } from 'react-dom'
 import { Cross } from '../icons/Cross'
@@ -11,21 +10,23 @@ export interface ModalProps {
   onClose: () => void
 }
 
-const Wrapper = styled(motion.div)`
+const Wrapper = styled('div')<{ isVisible: boolean }>`
   position: fixed;
   width: 100%;
-  height: 100%;
+  height: ${(props) => (props.isVisible ? '100%' : 0)};
   left: 0;
   top: 0;
   bottom: 0;
   right: 0;
   z-index: 3000;
+  visibility: ${(props) => (props.isVisible ? 'visible' : 'hidden')};
+  opacity: ${(props) => (props.isVisible ? 1 : 0)};
+  transition: opacity 350ms cubic-bezier(0.4, 0, 0.2, 1);
 `
 
-const Background = styled(motion.div)`
+const Background = styled('div')`
   position: fixed;
   width: 100%;
-  height: 100%;
   left: 0;
   top: 0;
   bottom: 0;
@@ -35,9 +36,10 @@ const Background = styled(motion.div)`
 
 interface ModalContainerProps {
   dynamicHeight?: boolean
+  isVisible: boolean
 }
 
-const ModalContainer = styled(motion.div)<ModalContainerProps>`
+const ModalContainer = styled('div')<ModalContainerProps>`
   position: relative;
   width: 100%;
   max-width: 62rem;
@@ -53,10 +55,15 @@ const ModalContainer = styled(motion.div)<ModalContainerProps>`
   position: absolute;
   left: 50%;
   top: 50%;
-  transform: translateX(-50%) translateY(-50%);
   box-shadow: 0 0 14px rgba(0, 0, 0, 0.06);
   box-sizing: border-box;
   overflow-x: scroll;
+  transition: opacity 350ms cubic-bezier(0.4, 0, 0.2, 1), transform 350ms cubic-bezier(0.4, 0, 0.2, 1);
+  opacity: ${(props) => (props.isVisible ? 1 : 0)};
+  transform: ${(props) =>
+    props.isVisible
+      ? 'translateX(-50%) translateY(-50%) scale(1)'
+      : 'translateX(-50%) translateY(30%) scale(0.9)'};
 
   @media (max-height: 900px) {
     max-height: calc(100vh - 2rem);
@@ -125,58 +132,11 @@ export const Modal: React.FC<ModalProps> = (props) => {
   }, [])
 
   return createPortal(
-    <Wrapper
-      initial={'hidden'}
-      animate={props.isVisible ? 'visible' : 'hidden'}
-      variants={{
-        visible: {
-          visibility: 'visible',
-        },
-        hidden: {
-          visibility: 'hidden',
-          transition: {
-            delay: 0.5,
-          },
-        },
-      }}
-    >
-      <Background
-        initial={'hidden'}
-        animate={props.isVisible ? 'visible' : 'hidden'}
-        variants={{
-          visible: {
-            opacity: 1,
-          },
-          hidden: {
-            opacity: 0,
-          },
-        }}
-      >
+    <Wrapper isVisible={props.isVisible}>
+      <Background>
         <ModalContainer
           dynamicHeight={props.dynamicHeight}
-          initial={'hidden'}
-          animate={props.isVisible ? 'visible' : 'hidden'}
-          transition={{
-            type: 'spring',
-            stiffness: 400,
-            damping: 100,
-          }}
-          variants={{
-            visible: {
-              opacity: 1,
-              transform: 'translateX(-50%) translateY(-50%) scale(1)',
-              transition: {
-                type: 'spring',
-                stiffness: 400,
-                damping: 100,
-                delay: 0.15,
-              },
-            },
-            hidden: {
-              opacity: 0,
-              transform: 'translateX(-50%) translateY(50%) scale(0.9)',
-            },
-          }}
+          isVisible={props.isVisible}
         >
           <ModalInnerContainer ref={containerRef}>
             {props.children}
