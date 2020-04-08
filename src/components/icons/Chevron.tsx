c46dcbe8b2e0af1c7a00a2bcb066954ae5a2ea48
gustaveen <gustaveen@gmail.com>
2020-04-08 19:39:32 +0200

    Remove react warning in svg

--- src/components/icons/Chevron.tsx
+++ src/components/icons/Chevron.tsx
@@ -9,7 +9,6 @@ export const Chevron: React.FunctionComponent<SvgProps> = ({
   <Svg viewBox="0 0 16 16" size={size} {...rest}>
     <path
       fill={fill}
-      fill-rule="evenodd"
       d="M8 10.873L1.127 4 0 5.127l7.437 7.436a.797.797 0 001.126 0L16 5.127 14.873 4 8 10.873z"
     />
   </Svg>
