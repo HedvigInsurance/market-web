7f6a57a38d9a518991a5c001f4a07a558ac2118b
Johan Palmfjord <johan.palmfjord@hedvig.com>
2020-06-12 11:01:22 +0200

    Implement correct redirection logic

--- src/server/middlewares/redirects.ts
+++ src/server/middlewares/redirects.ts
@@ -24,11 +24,17 @@ export const forceHost = ({
 export const startPageRedirect: IMiddleware<object> = async (ctx) => {
   const actualIp = ctx.ip
   const country = lookupCountry(actualIp)
+  const queryStringMaybe = ctx.querystring ? '?' + ctx.querystring : ''
 
-  if (country === 'NO') {
-    ctx.redirect(`/no${ctx.querystring ? '?' + ctx.querystring : ''}`)
+  if (!country || country === 'SE' || country === 'FI') {
+    ctx.redirect(`/se${queryStringMaybe}`)
     return
   }
 
-  ctx.redirect(`/se${ctx.querystring ? '?' + ctx.querystring : ''}`)
+  if (country === 'NO') {
+    ctx.redirect(`/no${queryStringMaybe}`)
+    return
+  }
+
+  ctx.redirect(`/se-en${queryStringMaybe}`)
 }
