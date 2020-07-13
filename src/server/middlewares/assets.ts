cf75d332207031a19b3880aa518a608361151666
Johan Palmfjord <johan.palmfjord@hedvig.com>
2020-07-13 18:45:32 +0200

    Clean up server entry a bit

---
+++ src/server/middlewares/assets.ts
@@ -0,0 +1,44 @@
+import Koa from 'koa'
+import mount from 'koa-mount'
+import proxy from 'koa-server-http-proxy'
+import koaStatic from 'koa-static'
+import path from 'path'
+
+export const configureAssets = (app: Koa) => {
+  const rootDir = path.resolve(__dirname, '../../..')
+  app.use(
+    mount(
+      '/assets-next',
+      koaStatic(path.resolve(rootDir, 'assets'), {
+        maxage: 1000 * 86400 * 365,
+        brotli: true,
+        gzip: true,
+      }),
+    ),
+  )
+
+  if (process.env.NODE_ENV === 'production') {
+    app.use(
+      mount(
+        '/static',
+        koaStatic(path.resolve(rootDir, 'build/static'), {
+          maxage: 1000 * 86400 * 365,
+          brotli: true,
+          gzip: true,
+        }),
+      ),
+    )
+  } else {
+    app.use(
+      proxy('/static', {
+        target: 'http://localhost:8031',
+      }),
+    )
+    app.use(
+      proxy('/sockjs-node', {
+        target: 'http://localhost:8031',
+        ws: true,
+      }),
+    )
+  }
+}
