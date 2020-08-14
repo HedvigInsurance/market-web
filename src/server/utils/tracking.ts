dd3d4f0877298dcc16a63c164924d61afd8ac300
Gustav <gustaveen@gmail.com>
2020-08-14 14:35:22 +0200

    Add nonce to gtm script

--- src/server/utils/tracking.ts
+++ src/server/utils/tracking.ts
@@ -18,10 +18,11 @@ const snap = `
 
 export const allTracking = (nonce: string) => `
 <!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+<script nonce="${nonce}">(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;var n=d.querySelector('[nonce]');
+n&&j.setAttribute('nonce',n.nonce||n.getAttribute('nonce'));f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-5BCDJ2B');</script>
 <!-- End Google Tag Manager -->
 
