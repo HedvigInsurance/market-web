f4aa13d0bd67e3f12dc8a751e404cbf6b73a0bce
Ellen Styrélius <ellen@hedvig.com>
2020-09-10 20:34:00 +0200

    Remove TODO from blockHelpers.tsx.

--- src/components/blockHelpers.tsx
+++ src/components/blockHelpers.tsx
@@ -24,7 +24,6 @@ export const LAPTOP_BP_UP = '@media (min-width: 1024px)'
 export const GIANT_BP_UP = '@media (min-width: 1700px)'
 
 export const SITE_MAX_WIDTH = {
-  // TODO: Decide whether it's a good idea to have this kind of global styling variables here (or at all)
   maxWidth: '100%',
 }
 
