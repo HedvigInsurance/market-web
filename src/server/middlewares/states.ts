83c217c5be7443a54e56b1368a7dc4525009cc11
gustaveen <gustaveen@gmail.com>
2020-09-14 12:26:20 +0200

    Remove teamtailor user fetching 🧹

--- src/server/middlewares/states.ts
+++ src/server/middlewares/states.ts
@@ -1,24 +1,6 @@
-import { IMiddleware } from 'koa-router'
 import { Logger } from 'typescript-logging'
-import { getCachedTeamtailorUsers } from '../utils/teamtailor'
 
 export interface State {
-  additionalStates?: {
-    teamtailorUsers?: { users: any }
-  }
-
   getLogger: (name: string) => Logger
   requestUuid?: string
-}
-
-export const addTeamtailorUsersToState: IMiddleware<State> = async (
-  ctx,
-  next,
-) => {
-  ctx.state.additionalStates = {
-    ...ctx.state.additionalStates,
-    teamtailorUsers: { users: await getCachedTeamtailorUsers() },
-  }
-
-  await next()
 }
