c1728243815c4ce134f56a66de57e814a40fd106
Ellen Styrélius <ellen@hedvig.com>
2020-09-24 15:59:26 +0200

    Add padding (3rem) to TextWrapper in ImageTextBlockBrandPivot - on padding-left if text is placed left of image and padding-right if text is placed to the right.

--- src/blocks/ImageTextBlockBrandPivot/index.tsx
+++ src/blocks/ImageTextBlockBrandPivot/index.tsx
@@ -79,8 +79,8 @@ const TextWrapper = styled('div')<{
   position: 'relative',
   textAlign: textPosition === 'center' ? 'center' : 'left',
   width: '100%',
-  paddingRight: textPosition === 'left' ? '7rem' : '0',
-  paddingLeft: textPosition === 'right' ? '7rem' : '0',
+  paddingRight: textPosition === 'left' ? '4rem' : '3rem',
+  paddingLeft: textPosition === 'right' ? '4rem' : '3rem',
   flexShrink: 1,
   [TABLET_BP_DOWN]: {
     paddingRight: textPosition === 'left' ? '3rem' : '0',
