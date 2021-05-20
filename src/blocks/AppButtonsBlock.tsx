3d8b0ed1723c3d1f001c4ce3471f557a9e0129cb
Robin Andeer <robin@hedvig.com>
2021-05-20 15:05:25 +0200

    Support standard color for app buttons block
    Pass color option to app buttons to turn them black on white (standard) background.
    Add type guard function to keep things type safe.

--- src/blocks/AppButtonsBlock.tsx
+++ src/blocks/AppButtonsBlock.tsx
@@ -1,7 +1,23 @@
 import React from 'react'
 import { AppButtons } from 'components/AppButtons/AppButtons'
 import { ContentWrapper, SectionWrapper } from 'components/blockHelpers'
-import { BrandPivotBaseBlockProps } from './BaseBlockProps'
+import {
+  BrandPivotBaseBlockProps,
+  minimalColorComponentColors,
+} from './BaseBlockProps'
+
+type StandardColor = 'standard' | 'standard-inverse'
+
+const STANDARD_COLORS: Array<minimalColorComponentColors> = [
+  'standard',
+  'standard-inverse',
+]
+
+function isStandardColor(
+  color: minimalColorComponentColors,
+): color is StandardColor {
+  return STANDARD_COLORS.includes(color)
+}
 
 export const AppButtonsBlock: React.FC<BrandPivotBaseBlockProps> = ({
   index,
@@ -15,7 +31,10 @@ export const AppButtonsBlock: React.FC<BrandPivotBaseBlockProps> = ({
     extraStyling={extra_styling}
   >
     <ContentWrapper brandPivot index={index}>
-      <AppButtons alignCenter />
+      <AppButtons
+        alignCenter
+        color={color && isStandardColor(color.color) ? color.color : undefined}
+      />
     </ContentWrapper>
   </SectionWrapper>
 )
