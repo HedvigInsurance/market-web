aafd3ab17b3d4f2f72e59bc5205ade80c6fd0419
Robin Andeer <robin@hedvig.com>
2021-07-27 14:08:17 +0200

    Migrate from enzyme -> testing library

--- jest.config.js
+++ jest.config.js
@@ -5,5 +5,4 @@ module.exports = {
   testRegex: '\\.test\\.(ts|tsx|js)$',
   moduleFileExtensions: ['ts', 'tsx', 'js'],
   moduleDirectories: ['node_modules', 'src'],
-  setupFiles: ['<rootDir>/test-setup-enzyme.js'],
 }
--- test-setup-enzyme.js
+++
@@ -1,5 +0,0 @@
-const { configure } = require('enzyme')
-const Adapter = require('enzyme-adapter-react-16')
-require('jest')
-
-configure({ adapter: new Adapter() })
