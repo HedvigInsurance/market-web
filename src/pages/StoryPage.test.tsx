202e34007c454ac2103acab0ea3763d23c4a08f9
Robin Andeer <robin.andeer@gmail.com>
2021-08-11 14:15:54 +0200

    Migrate from enzyme -> testing library (#591)

--- src/pages/StoryPage.test.tsx
+++ src/pages/StoryPage.test.tsx
@@ -1,30 +1,7 @@
-import { Provider } from 'constate'
-import { mount } from 'enzyme'
 import React from 'react'
-import { HelmetProvider } from 'react-helmet-async'
-import { BrowserRouter } from 'react-router-dom'
-import { v4 as uuidV4 } from 'uuid'
+import { renderComponent } from '../test-utils'
 import { StoryPage } from './StoryPage'
 
 test('renders without 💥', () => {
-  mount(
-    <Provider
-      initialState={{
-        story: {
-          story: {
-            uuid: uuidV4(),
-            name: 'blargh',
-            full_slug: '/home',
-            content: { _uid: uuidV4(), body: [] },
-          },
-        },
-      }}
-    >
-      <BrowserRouter>
-        <HelmetProvider>
-          <StoryPage />
-        </HelmetProvider>
-      </BrowserRouter>
-    </Provider>,
-  )
+  renderComponent(<StoryPage />)
 })
---
+++ src/test-utils.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { render, RenderOptions } from '@testing-library/react'
+import { Provider } from 'constate'
+import { HelmetProvider } from 'react-helmet-async'
+import { v4 as uuidV4 } from 'uuid'
+import { BrowserRouter } from 'react-router-dom'
+
+const initialState = {
+  story: {
+    story: {
+      uuid: uuidV4(),
+      name: 'blargh',
+      full_slug: '/home',
+      content: { _uid: uuidV4(), body: [] },
+    },
+  },
+}
+
+const AllTheProviders: React.FC = ({ children }) => {
+  return (
+    <Provider initialState={initialState}>
+      <BrowserRouter>
+        <HelmetProvider>{children}</HelmetProvider>
+      </BrowserRouter>
+    </Provider>
+  )
+}
+
+const customRender = (
+  ui: React.ReactElement,
+  options?: Omit<RenderOptions, 'wrapper'>,
+) => render(ui, { wrapper: AllTheProviders, ...options })
+
+export * from '@testing-library/react'
+export { customRender as renderComponent }
