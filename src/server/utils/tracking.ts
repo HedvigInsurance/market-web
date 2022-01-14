9aa3d3b1e83418d9874880e0ef706d86ca3490c6
Gustav Eén <gustav.een@hedvig.com>
2022-01-14 16:00:14 +0100

    Add seperate GTM scripts for staging/dev and prod (#624)
    This enables us to deploy and test tracking in staging a lot easier

--- src/server/utils/tracking.ts
+++ src/server/utils/tracking.ts
@@ -16,18 +16,43 @@ const snap = `
     snaptr('track','PAGE_VIEW')
 `
 
-export const allTracking = (nonce: string) => `
+export const gtmDevScript = {
+  head: `<!-- Google Tag Manager -->
+  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl+ '&gtm_auth=JjXRUaZZaACoAL4xQQE0lw&gtm_preview=env-386&gtm_cookies_win=x';f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-5BCDJ2B');</script>
+  <!-- End Google Tag Manager -->`,
+  body: `<!-- Google Tag Manager (noscript) -->
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5BCDJ2B&gtm_auth=JjXRUaZZaACoAL4xQQE0lw&gtm_preview=env-386&gtm_cookies_win=x"
+  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+  <!-- End Google Tag Manager (noscript) -->`,
+}
+
+export const gtmProdScript = {
+  head: `<!-- Google Tag Manager -->
+  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl+ '&gtm_auth=oslbzY6jCtH0Y2AoZVnZ2A&gtm_preview=env-2&gtm_cookies_win=x';f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-5BCDJ2B');</script>
+  <!-- End Google Tag Manager -->`,
+  body: `<!-- Google Tag Manager (noscript) -->
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5BCDJ2B&gtm_auth=oslbzY6jCtH0Y2AoZVnZ2A&gtm_preview=env-2&gtm_cookies_win=x"
+  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+  <!-- End Google Tag Manager (noscript) -->`,
+}
+
+export const allTracking = (
+  nonce: string,
+  appEnvironment: string = 'development',
+) => `
 <script nonce="${nonce}">
   dataLayer = [];
 </script>
 
-<!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-5BCDJ2B');</script>
-<!-- End Google Tag Manager -->
+${appEnvironment === 'production' ? gtmProdScript.head : gtmDevScript.head}
 
 <script nonce="${nonce}">
 ${[snap].join(';\n')}
