c307b3e72eae10ad603d815a0e05cdf3833d02a2
Gustav Eén <gustaveen@gmail.com>
2022-05-03 15:07:07 +0200

    Update PerilCollection component and creat list component

--- src/components/Perils/PerilCollection/PerilCollection.tsx
+++ src/components/Perils/PerilCollection/PerilCollection.tsx
@@ -1,63 +1,37 @@
-import styled from '@emotion/styled'
-import React from 'react'
-import { LAPTOP_BP_UP, TABLET_BP_UP } from 'components/blockHelpers'
-import { PerilItem } from 'components/Perils/PerilItem/PerilItem'
-import { minimalColorComponentColors } from 'src/blocks/BaseBlockProps'
-import { Peril } from '../types'
+import React, { useState } from 'react'
+import { minimalColorComponentColors } from 'blocks/BaseBlockProps'
+import { GlobalStory } from 'storyblok/StoryContainer'
+import { PerilList } from 'components/Perils/PerilList/PerilList'
+import { PerilModal } from 'components/Perils/PerilModal/PerilModal'
+import { Peril } from 'components/Perils/types'
 
-interface Props {
+type Props = {
   color: minimalColorComponentColors
-  perils: ReadonlyArray<Peril>
-  setCurrentPeril: (index: number) => void
-  setIsShowingPeril: (isShowingPeril: boolean) => void
+  perils: Peril[]
+  story: GlobalStory
 }
 
-const Wrapper = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  margin-left: -0.5rem;
-
-  ${TABLET_BP_UP} {
-    margin-left: -1rem;
-  }
-
-  & > * {
-    width: calc(50% - 0.5rem);
-    margin-left: 0.5rem;
-    margin-bottom: 0.5rem;
-
-    ${TABLET_BP_UP} {
-      width: calc(100% / 3 - 1rem);
-      margin-left: 1rem;
-      margin-bottom: 1rem;
-    }
-
-    ${LAPTOP_BP_UP} {
-      width: calc(25% - 1rem);
-    }
-  }
-`
-
-export const PerilCollection: React.FC<Props> = ({
-  color,
-  perils,
-  setCurrentPeril,
-  setIsShowingPeril,
-}) => (
-  <Wrapper>
-    {perils.map((peril, perilIndex) => (
-      <PerilItem
-        key={peril.title?.toString() || 'unknown'}
+export const PerilCollection: React.FC<Props> = ({ perils, story, color }) => {
+  const [isShowingPeril, setIsShowingPeril] = useState(false)
+  const [currentPeril, setCurrentPeril] = useState(0)
+  return (
+    <>
+      <PerilList
         color={color}
-        title={peril.title}
-        icon={peril.icon}
-        onClick={() => {
-          setCurrentPeril(perilIndex)
-          setIsShowingPeril(true)
-        }}
+        perils={perils}
+        setCurrentPeril={setCurrentPeril}
+        setIsShowingPeril={setIsShowingPeril}
       />
-    ))}
-  </Wrapper>
-)
+      {perils.length > 0 && (
+        <PerilModal
+          perils={perils}
+          currentPerilIndex={currentPeril}
+          setCurrentPeril={setCurrentPeril}
+          isVisible={isShowingPeril}
+          story={story}
+          onClose={() => setIsShowingPeril(false)}
+        />
+      )}
+    </>
+  )
+}
