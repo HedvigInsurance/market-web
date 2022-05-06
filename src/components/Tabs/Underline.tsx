893f83e46aed5167663d597cc717e55a9f996f83
Gustav Eén <gustaveen@gmail.com>
2022-05-06 10:39:09 +0200

    Fix responsive styling for Underline

--- src/components/Tabs/Underline.tsx
+++ src/components/Tabs/Underline.tsx
@@ -1,25 +1,24 @@
 import React from 'react'
 import { motion } from 'framer-motion'
 import { colorsV3 } from '@hedviginsurance/brand'
-// import { useMediaQuery } from 'react-responsive'
-// import { BREAKPOINTS } from 'utils/mediaQueries'
+import styled from '@emotion/styled'
+import { LAPTOP_BP_UP } from '../blockHelpers'
 
 export const UNDERLINE_HEIGHT = '2px'
 
-export const UnderlineComponent: React.FC = () => {
-  // const isDesktop = useMediaQuery({ minWidth: BREAKPOINTS.mediumScreen })
+const Line = styled(motion.div)`
+  height: ${UNDERLINE_HEIGHT};
+  background-color: ${colorsV3.gray900};
+  position: absolute;
+  bottom: 0;
+  width: calc(100% - 1rem);
+  z-index: 1;
 
-  return (
-    <motion.div
-      layoutId="underline"
-      style={{
-        height: UNDERLINE_HEIGHT,
-        backgroundColor: colorsV3.gray900,
-        position: 'absolute',
-        bottom: 0,
-        width: 'calc(100% - 2.5rem)',
-        zIndex: 1,
-      }}
-    />
-  )
+  ${LAPTOP_BP_UP} {
+    width: calc(100% - 2.5rem);
+  }
+`
+
+export const UnderlineComponent: React.FC = () => {
+  return <Line layoutId="underline" />
 }
