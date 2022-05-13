075fab8cb0a71431c83222777e4bdee68f2fa9ec
Gustav Eén <gustaveen@gmail.com>
2022-05-13 15:47:18 +0200

    Add title and text fields

--- src/blocks/TableBlock/TableBlock.tsx
+++ src/blocks/TableBlock/TableBlock.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { colorsV3 } from '@hedviginsurance/brand'
+import styled from '@emotion/styled'
 import {
   Table,
   TableBody,
@@ -7,23 +8,60 @@ import {
   TableHead,
   TableRow,
 } from 'components/Table/Table'
-import { SectionWrapper, ContentWrapper } from 'components/blockHelpers'
+import {
+  SectionWrapper,
+  ContentWrapper,
+  TABLET_BP_UP,
+} from 'components/blockHelpers'
 import { Tick } from 'components/icons/Tick'
-import { Dash } from 'src/components/icons/Dash'
+import { Dash } from 'components/icons/Dash'
+import { Heading } from 'components/Heading/Heading'
 import { BaseBlockProps } from '../BaseBlockProps'
 
 type TableBlockProps = BaseBlockProps & {
+  title?: string
+  text?: string
   table: any
 }
+
+const TableHeader = styled.div({
+  '&:not(:empty)': {
+    marginBottom: '1rem',
+
+    [TABLET_BP_UP]: {
+      marginBottom: '2rem',
+    },
+  },
+})
+
+const Text = styled.p({
+  fontSize: '1rem',
+  textAlign: 'center',
+})
 
 const getCellIcon = (cell: string) => {
   return Number(cell) ? <Tick /> : <Dash fill={colorsV3.gray400} />
 }
 
-export const TableBlock = ({ color, index, size, table }: TableBlockProps) => {
+export const TableBlock = ({
+  color,
+  index,
+  text,
+  size,
+  title,
+  table,
+}: TableBlockProps) => {
   return (
     <SectionWrapper colorComponent={color} size={size} brandPivot>
       <ContentWrapper brandPivot index={index}>
+        <TableHeader>
+          {title && (
+            <Heading as="h3" mobileSize="sm" size="xs" textPosition="center">
+              {title}
+            </Heading>
+          )}
+          {text && <Text>{text}</Text>}
+        </TableHeader>
         <Table fullWidth>
           {table.thead && (
             <TableHead>
