a1c8e936425b07ca5512b8cb2d25a5c405139535
Gustav Eén <gustaveen@gmail.com>
2022-11-28 09:56:40 +0100

    Use native lazy loading for images

--- src/components/DeferredImage.tsx
+++ src/components/DeferredImage.tsx
@@ -1,20 +1,5 @@
 import styled from '@emotion/styled'
-import React, { createRef, MutableRefObject, PureComponent } from 'react'
-import VisibilitySensor from 'react-visibility-sensor'
-
-interface State {
-  width?: number
-  height?: number
-  ref: React.Ref<HTMLImageElement>
-  isLoaded?: boolean
-}
-
-const Img = styled('img')<{ isVisible: boolean }>(({ isVisible }) => ({
-  opacity: isVisible ? 1 : 0,
-  transition: 'opacity 300ms',
-  maxWidth: '100%',
-  height: 'auto',
-}))
+import React from 'react'
 
 const getSizeFromURL = (url: string) => {
   const [, rawWidth, rawHeight] = url.match(/\/(\d+)x(\d+)\//) || []
@@ -24,67 +9,17 @@ const getSizeFromURL = (url: string) => {
   return { width, height }
 }
 
-const stateHasRef = (
-  state: State,
-): state is { ref: React.RefObject<HTMLImageElement> } =>
-  Boolean(typeof state.ref === 'object' && state.ref && state.ref.current)
+const Img = styled.img({
+  maxWidth: '100%',
+  height: 'auto',
+})
 
-export class DeferredImage extends PureComponent<
-  React.DetailedHTMLProps<
-    React.ImgHTMLAttributes<HTMLImageElement>,
-    HTMLImageElement
-  > & { imageRef?: MutableRefObject<HTMLImageElement | null> | null },
-  State
-> {
-  public state: State = {
-    width: undefined,
-    height: undefined,
-    ref: this.props.imageRef || createRef<HTMLImageElement>(),
-    isLoaded: false,
-  }
-
-  public render() {
-    const sizeProps = this.props.src ? getSizeFromURL(this.props.src) : ''
-    return (
-      <VisibilitySensor
-        offset={{ top: -200, bottom: 1 }}
-        partialVisibility
-        onChange={(isVisible) => {
-          if (isVisible) {
-            const refObject = stateHasRef(this.state)
-              ? this.state.ref.current
-              : null
-            if (refObject) {
-              refObject.addEventListener('load', this.handleSizeChange)
-            }
-          }
-        }}
-      >
-        {({ isVisible }) => (
-          <Img
-            {...this.props}
-            {...sizeProps}
-            loading="lazy"
-            src={this.props.src}
-            ref={this.state.ref}
-            isVisible={isVisible ?? false}
-          />
-        )}
-      </VisibilitySensor>
-    )
-  }
-
-  private handleSizeChange = () => {
-    this.setState((state) => {
-      const refObject = stateHasRef(state) ? state.ref.current : null
-      if (refObject) {
-        return {
-          height: refObject.scrollHeight,
-          width: refObject.scrollWidth,
-        }
-      }
-
-      return {}
-    })
-  }
+export const DeferredImage = ({
+  src,
+}: React.DetailedHTMLProps<
+  React.ImgHTMLAttributes<HTMLImageElement>,
+  HTMLImageElement
+>) => {
+  const sizeProps = src ? getSizeFromURL(src) : ''
+  return <Img {...sizeProps} loading="lazy" src={src} />
 }
