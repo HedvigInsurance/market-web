a469b163ef75848552347f657b3e21f4c5d5c3c4
Gustav Eén <gustaveen@gmail.com>
2023-01-19 10:13:10 +0100

    Use storybloks pagination to load more articles

--- src/blocks/ArticleListBlock/ArticleListBlock.tsx
+++ src/blocks/ArticleListBlock/ArticleListBlock.tsx
@@ -4,6 +4,7 @@ import styled from '@emotion/styled'
 import { useLocale } from 'context/LocaleContext'
 import { ContentWrapper, SectionWrapper } from 'components/blockHelpers'
 import { ArticleStory } from 'src/storyblok/StoryContainer'
+import { Button } from 'components/Button/Button'
 import { BaseBlockProps } from '../BaseBlockProps'
 import { ArticleTeaser } from './ArticleTeaser'
 
@@ -15,16 +16,21 @@ export const ArticleListBlock = ({ category }: ArticleListBlockProps) => {
   const {
     currentLocale: { label },
   } = useLocale()
-  const [articles, setArticles] = useState([])
+  const [articles, setArticles] = useState<ArticleStory[]>([])
+  const [page, setPage] = useState(1)
+  const [totalPages, setTotalPages] = useState(1)
+  const pageSize = 6
 
   useEffect(() => {
     const getArticles = async () => {
       try {
         const storyblokApi = getStoryblokApi()
-        const { data } = await storyblokApi.get(`cdn/stories/`, {
+        const { data, total } = await storyblokApi.get(`cdn/stories/`, {
           starts_with: `${label}`,
           content_type: 'article',
           resolve_relations: 'article.categories',
+          per_page: pageSize,
+          page: page,
           ...(category && {
             filter_query: {
               categories: {
@@ -34,13 +40,21 @@ export const ArticleListBlock = ({ category }: ArticleListBlockProps) => {
           }),
         })
 
-        setArticles(data.stories)
+        const totalPages = Math.ceil(total / pageSize)
+        setTotalPages(totalPages)
+        setArticles((previousArticles) => {
+          return [...previousArticles, ...data.stories]
+        })
       } catch (error) {
         setArticles([])
       }
     }
     getArticles()
-  }, [label, category])
+  }, [label, category, page])
+
+  const handleClick = () => {
+    setPage((prev) => prev + 1)
+  }
 
   return (
     <SectionWrapper brandPivot>
@@ -52,13 +66,23 @@ export const ArticleListBlock = ({ category }: ArticleListBlockProps) => {
             </Link>
           ))}
         </Grid>
+        {page < totalPages && (
+          // TODO: Translate button label
+          <Button onClick={handleClick}>Fler nyheter</Button>
+        )}
       </Wrapper>
     </SectionWrapper>
   )
 }
 
 const Wrapper = styled(ContentWrapper)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 4rem;
   max-width: 84rem;
+  min-height: 60vh;
+  margin-inline: auto;
 `
 
 const Grid = styled.div`
