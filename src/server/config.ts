0e3f9a8f67ecf868ea57d764918895a4cf4a7683
Alexander Lebedev <alexander.lebedev@gmail.com>
2023-06-14 10:57:45 +0200

    Use HEROKU_REDIST_WHITE_TLS_URL for staging (#726)
    <!-- 
    To link the branch/PR to a Jira issue either
    1. (preferred) add the issue key to the name of your branch (e.g. GRW-123/fix/some-annoying-bug)
    2. prefix your PR title with it
    
    Also, if applicable, include whether this is a Fix, Feature or Chore.
    
    Example PR title: GRW-123 / Feature / Awesome new thing
    -->
    
    ## What?
    
    <!-- What changes are made? If there are many changes, a list might be a good format. -->
    
    Try using Heroku-specific redis URL for staging first so that we don't need to update REDIS_URL manually
    
    ## Why?
    
    <!-- Why are these changes made? -->
    
    Don't want to fix staging settings manually now and then
    
    
    **Ticket(s): []**
    <!-- If there is a Jira issue, add the key (e.g. GRW-123) between the brackets, and a link to that issue will automatically be created. -->
    
    
    <!-- If it makes sense, add screenshots and/or screen recordings below, with headlines and/or descriptions if needed. -->
    <!--
    ## Screenshots / recordings 
    -->
    
    <!-- Finally, you can create a review app on Heroku to make it easier to review and/or get input from the design team before merging. -->
    <!--
    ### [Review app]()
    -->

--- src/server/config.ts
+++ src/server/config.ts
@@ -11,7 +11,8 @@ export interface Config {
 }
 
 export const config: Config = {
-  redisUrl: process.env.REDIS_URL,
+  // Staging config first, production and local as fallback
+  redisUrl: process.env.HEROKU_REDIS_WHITE_TLS_URL || process.env.REDIS_URL,
   forceHost: process.env.FORCE_HOST,
   teamtailorApiToken: process.env.TEAMTAILOR_API_TOKEN,
   storyblokApiToken: process.env.STORYBLOK_API_TOKEN!,
