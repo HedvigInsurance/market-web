be6164b9a463a53b304c1fc7f86839a3b01bf084
Robin Andeer <robin.andeer@gmail.com>
2023-09-14 13:49:31 +0200

    Remove 404 illustration

--- src/blocks/FourOhFourBlock.tsx
+++ src/blocks/FourOhFourBlock.tsx
@@ -1,60 +1,12 @@
 import styled from '@emotion/styled'
 import React from 'react'
-import { Claims } from 'components/illustrations/Claims'
 import { GlobalStoryContainer } from 'storyblok/StoryContainer'
 import {
   ContentWrapper,
   LAPTOP_BP_UP,
   MOBILE_BP_UP,
   SectionWrapper,
-  TABLET_BP_UP,
 } from '../components/blockHelpers'
-
-const Title = styled.h1`
-  font-size: 2rem;
-  text-align: center;
-
-  ${MOBILE_BP_UP} {
-    font-size: 3rem;
-  }
-
-  ${LAPTOP_BP_UP} {
-    font-size: 4rem;
-  }
-`
-
-const IllustrationWrapper = styled.div`
-  max-width: 490px;
-  width: 80%;
-  margin: 6rem auto 2rem;
-  padding-left: 1rem;
-  padding-right: 1rem;
-
-  ${MOBILE_BP_UP} {
-    width: 70%;
-    margin-bottom: 3rem;
-  }
-
-  ${TABLET_BP_UP} {
-    margin-top: 15vh;
-  }
-
-  ${LAPTOP_BP_UP} {
-    margin-bottom: 4.5rem;
-    font-size: 4rem;
-  }
-`
-const Illustration = styled.div`
-  position: relative;
-  height: 0;
-  padding-top: 81%;
-
-  svg {
-    position: absolute;
-    top: 0;
-    left: 0;
-  }
-`
 
 export const FourOhFourBlock: React.FunctionComponent = () => (
   <SectionWrapper
@@ -64,17 +16,31 @@ export const FourOhFourBlock: React.FunctionComponent = () => (
       plugin: 'hedvig_minimal_color_picker',
     }}
   >
-    <ContentWrapper>
-      <IllustrationWrapper>
-        <Illustration>
-          <Claims />
-        </Illustration>
-      </IllustrationWrapper>
+    <StyledContentWrapper>
       <GlobalStoryContainer>
-        {({ globalStory }) => {
-          return <Title>{globalStory.content.four_oh_four_title}</Title>
-        }}
+        {({ globalStory }) => (
+          <Title>{globalStory.content.four_oh_four_title}</Title>
+        )}
       </GlobalStoryContainer>
-    </ContentWrapper>
+    </StyledContentWrapper>
   </SectionWrapper>
 )
+
+const StyledContentWrapper = styled(ContentWrapper)`
+  min-height: 75vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+const Title = styled.h1`
+  font-size: 2rem;
+
+  ${MOBILE_BP_UP} {
+    font-size: 3rem;
+  }
+
+  ${LAPTOP_BP_UP} {
+    font-size: 4rem;
+  }
+`
